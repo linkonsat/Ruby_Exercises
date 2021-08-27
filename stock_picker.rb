6fe1e5a3dc5796257760d67c40fb6111e6908199
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-08-26 23:04:06 -0700

    pretty major progress on the stock picker. the day is correct and it shows the profit. however it doesnt give the right index for the sale date. need to change that next.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,6 +1,17 @@
 
 def stock_picker(daily_prices)
+    profit_array = [0,0,0]
+   new_prices = daily_prices.each_with_index { | day, index |  
+    price_comparison = daily_prices[index, daily_prices.length].map { |num| num - day}
+    if ( price_comparison.max > profit_array[2]) 
+    profit_array.replace([index,daily_prices.length - price_comparison.index(price_comparison.max),price_comparison.max])
+    end
+    }
 
+    p profit_array
+   ## so first were gonna need to iterate over the days to actually get the daily_prices
+    #then once we have the day we minus the sell price from the buy price. which using filter
+   # we could return a nested array of the buy and sell day and the profits and then return the nested array which contains the highest number
 
 end
 
